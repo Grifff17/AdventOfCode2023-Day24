0bdc6a34a9b6b68814d68a22df1b186b197f5984
Griffin Riley <rileyg@oregonstate.edu>
2024-04-10 12:10:08 -0700

    solved part 1

--- solution.py
+++ solution.py
@@ -1,6 +1,60 @@
 
 def solvepart1():
-    data = fileRead("test.txt")
+    #read in data
+    data = fileRead("input.txt")
+    hailstones = []
+    for row in data:
+        firstHalf, secondHalf = row.strip().replace(" ","").split("@")
+        pos = [ int(val) for val in firstHalf.split(",") ]
+        vel = [ int(val) for val in secondHalf.split(",") ]
+        hailstones.append((pos,vel))
+    
+    #calculate intersections
+    sum = 0
+    minval = 200000000000000
+    maxval = 400000000000000
+    for i in range(len(hailstones)):
+        h1 = hailstones[i]
+        for j in range(i+1, len(hailstones)):
+            h2 = hailstones[j]
+            s1, i1 = getSlopeAndIntercept2D(h1[0],h1[1])
+            s2, i2 = getSlopeAndIntercept2D(h2[0],h2[1])
+            intersects, intersectPos = getIntersection2D(s1, i1, s2, i2)
+            if intersects and inRange2D(intersectPos, minval, maxval) and posInFuture2D(h1[0], h1[1], intersectPos) and posInFuture2D(h2[0], h2[1], intersectPos):
+                sum += 1
+    print(sum)
+
+
+#finds the intersection of two 2d lines, returns false if line are parallel
+#input: ax + c = y for each line
+def getIntersection2D(a1,c1,a2,c2):
+    if a1 == a2:
+        return False, (0,0)
+    b1 = -1
+    b2 = -1
+    x = ((b1*c2)-(b2*c1))/((a1*b2)-(a2*b1))
+    y = ((c1*a2)-(c2*a1))/((a1*b2)-(a2*b1))
+    return True, (x,y)
+
+#gets the slope and y intercept of a 2d line from a position and velocity
+def getSlopeAndIntercept2D(pos, vel):
+    slope = vel[1] / vel[0]
+    intercept = pos[1] - (pos[0]*slope)
+    return slope, intercept
+
+#checks whether a 2d point is in a range in both x and y
+def inRange2D(pos, least, most):
+    return pos[0] >= least and pos[0] <= most and pos[1] >= least and pos[1] <= most
+
+#calculates whether a point is before or after another point on a graph with a given velocity, in 2d
+def posInFuture2D(oldPos, velocity, newPos):
+    valid = True
+    if (oldPos[0] > newPos[0]) == (velocity[0] > 0):
+        valid = False
+    if (oldPos[1] > newPos[1]) == (velocity[1] > 0):
+        valid = False
+    return valid
+
 
 def fileRead(name):
     data = []
